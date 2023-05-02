4aef6897776f6e070a0196e44eb35839e2484590
Aditya Gupta <84224144+guptaaditya2122@users.noreply.github.com>
2023-05-02 15:27:13 +0530

    Update Skill.tsx

--- components/Skills/SkillCard/Skill.tsx
+++ components/Skills/SkillCard/Skill.tsx
@@ -31,7 +31,7 @@ export default function Skill({skillType,directionLeft}: Props) {
       <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
       group-hover:bg-white h-16 w-16 md:h-20 md:w-20 xl:w-22 xl:h-22 rounded-full z-0'>
         <div className='flex items-center justify-center h-full'>
-            <p className='text-3xl font-bold text-black opacity-100'>{skillType?.progress}</p>
+            <p className='text-3xl font-bold text-black opacity-100'>{skillType?.progress}%</p>
         </div>
       </div>
     </div>
